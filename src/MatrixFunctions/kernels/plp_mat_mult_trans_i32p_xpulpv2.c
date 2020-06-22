e866c153ffc1ec796e95955be31ffd1dba6cce18
Tibor Schneider <tiborschneider@bluewin.ch>
2020-06-22 13:26:59 +0200

    Improved plp_mat_mult_trans_i32p_xpulpv2

--- src/MatrixFunctions/kernels/plp_mat_mult_trans_i32p_xpulpv2.c
+++ src/MatrixFunctions/kernels/plp_mat_mult_trans_i32p_xpulpv2.c
@@ -123,36 +123,132 @@ void plp_mat_mult_trans_i32p_xpulpv2( void* args) {
         uint32_t nPE = arguments->nPE;
         int32_t * __restrict__ pDstC = arguments->pDstC;
 
-        uint32_t i; // loop counter for M
-        uint32_t j; // loop counter for N
-        uint32_t k; // loop counter for O
+        uint32_t m; // loop counter for M
+        uint32_t n; // loop counter for N
+        uint32_t o; // loop counter for O
 
         int core_id = rt_core_id();
 
-        if(N & 0x1){
-          for(i=core_id; i < M; i += nPE){
-            for(k=0; k < O; k++){
-              int32_t sum1 = 0;
-              int32_t sum2 = 0;
-              for(j=0; j<N/2; j++){
-                sum1 = sum1 + pSrcA[i*N + j*2]*pSrcB[k*N+j*2];
-                sum2 = sum2 + pSrcA[i*N + j*2+1]*pSrcB[k*N+j*2+1];
-              }
-              pDstC[i*O +k] = sum1 + sum2 + pSrcA[i*N + j*2]*pSrcB[k*N+j*2];
+        // first, to the major part of the computation:
+        //for (m = core_id * 2; m < M; m += nPE * 2) {
+        m = core_id * 2;
+        do {
+            //for (o = 0; o < O; o += 2) {
+            o = 0;
+            do {
+                int32_t sum_m0_o0 = 0;
+                int32_t sum_m0_o1 = 0;
+                int32_t sum_m1_o0 = 0;
+                int32_t sum_m1_o1 = 0;
+                //for (n = 0; n < N; n++) {
+                n = 0;
+                do {
+                    // load all 4 values
+                    int32_t a_m0 = pSrcA[m * N + n];
+                    int32_t a_m1 = pSrcA[(m + 1) * N + n];
+                    int32_t b_o0 = pSrcB[o * N + n];
+                    int32_t b_o1 = pSrcB[(o + 1) * N + n];
+
+                    // compute all 4 macs
+                    sum_m0_o0 += a_m0 * b_o0;
+                    sum_m0_o1 += a_m0 * b_o1;
+                    sum_m1_o0 += a_m1 * b_o0;
+                    sum_m1_o1 += a_m1 * b_o1;
+
+                    n++;
+                } while (n < N);
+
+                // store the values
+                pDstC[m * O + o]           = sum_m0_o0;
+                pDstC[m * O + o + 1]       = sum_m0_o1;
+                pDstC[(m + 1) * O + o]     = sum_m1_o0;
+                pDstC[(m + 1) * O + o + 1] = sum_m1_o1;
+
+                o += 2;
+            } while (o < O - 1);
+
+            m += nPE * 2;
+        } while (m < M - 1);
+
+        // clean up the odd O (o = O - 1)
+        if (O & 0x1) {
+            o = O - 1;
+            // loop over all m, unrolling two elements
+            //for (m = core_id * 2; m < M - 1; m += nPE * 2) {
+            m = core_id * 2;
+            do {
+                int32_t sum_m0 = 0;
+                int32_t sum_m1 = 0;
+                for (n = 0; n < N; n++) {
+                    // load all 3values
+                    int32_t b    = pSrcB[o * N + n];
+                    int32_t a_m0 = pSrcA[m * N + n];
+                    int32_t a_m1 = pSrcA[(m + 1) * N + n];
+                    
+                    // compute the two MACs
+                    sum_m0 += a_m0 * b;
+                    sum_m1 += a_m1 * b;
+                }
+                pDstC[m * O + o]       = sum_m0;
+                pDstC[(m + 1) * O + o] = sum_m1;
+
+                m += nPE * 2;
+            } while (m < M - 1);
+        }
+
+        // clean up the odd M (m = M - 1)
+        if (M & 0x1) {
+            m = M - 1;
+            // loop over all m, unrolling two elements
+            //for (o = core_id * 2; o < O - 1; o += nPE * 2) {
+            o = core_id * 2;
+            do {
+                int32_t sum_o0 = 0;
+                int32_t sum_o1 = 0;
+                for (n = 0; n < N; n++) {
+                    // load all 3values
+                    int32_t a    = pSrcA[m * N + n];
+                    int32_t b_o0 = pSrcB[o * N + n];
+                    int32_t b_o1 = pSrcB[(o + 1) * N + n];
+                    
+                    // compute the two MACs
+                    sum_o0 += a * b_o0;
+                    sum_o1 += a * b_o1;
+                }
+                pDstC[m * O + o]     = sum_o0;
+                pDstC[m * O + o + 1] = sum_o1;
+
+                o += nPE * 2;
+            } while (o < O - 1);
+        }
+
+        // clean up the odd M and odd O (m = M - 1, o = O - 1)
+        if ((M & 0x1) && (O & 0x1) && core_id == nPE - 1)  {
+            m = M - 1;
+            o = O - 1;
+            int32_t sum = 0;
+
+            //for (n = core_id * 2; n < N; n += nPE * 2) {
+            n = 0;
+            do {
+                int32_t a0 = pSrcA[m * N + n];
+                int32_t a1 = pSrcA[m * N + n + 1];
+                int32_t b0 = pSrcB[o * N + n];
+                int32_t b1 = pSrcB[o * N + n + 1];
+
+                // compute the dot product
+                sum += a0 * b0 + a1 * b1;
+
+                n += 2;
+            } while (n < N - 1);
+
+            // if N is odd, we need to add the last element to the sum
+            if (N & 0x1) {
+                // N is odd
+                n = N - 1;
+                sum += pSrcA[m * N + n] * pSrcB[o * N + n];
             }
-          }
-        } else {
-          for(i=core_id; i < M; i += nPE){
-            for(k=0; k < O; k++){
-              int32_t sum1 = 0;
-              int32_t sum2 = 0;
-              for(j=0; j<N/2; j++){
-                sum1 = sum1 + pSrcA[i*N + j*2]*pSrcB[k*N+j*2];
-                sum2 = sum2 + pSrcA[i*N + j*2+1]*pSrcB[k*N+j*2+1];
-              }
-              pDstC[i*O +k] = sum1 + sum2;
-            }
-          }
+            pDstC[m * O + o] = sum;
         }
 
         rt_team_barrier();
