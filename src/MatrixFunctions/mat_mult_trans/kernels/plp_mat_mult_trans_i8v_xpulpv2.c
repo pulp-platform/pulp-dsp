d4e8f4da409cfb6d6fb5f41c82c3f358ae4b1188
Tibor Schneider <tiborschneider@bluewin.ch>
2020-07-30 19:35:19 +0200

    Fixed mat_mult_trans_i8 on xpulpv2

--- src/MatrixFunctions/mat_mult_trans/kernels/plp_mat_mult_trans_i8v_xpulpv2.c
+++ src/MatrixFunctions/mat_mult_trans/kernels/plp_mat_mult_trans_i8v_xpulpv2.c
@@ -50,10 +50,6 @@
   @return     none
  */
 
-// define BASIC_VERSION // if used don't forget to also use the undefine at end of file
-
-#ifdef BASIC_VERSION
-
 void plp_mat_mult_trans_i8v_xpulpv2(const int8_t *__restrict__ pSrcA,
                                     const int8_t *__restrict__ pSrcB,
                                     uint32_t M,
@@ -61,202 +57,31 @@ void plp_mat_mult_trans_i8v_xpulpv2(const int8_t *__restrict__ pSrcA,
                                     uint32_t O,
                                     int32_t *__restrict__ pDstC) {
 
-    uint32_t i; // loop counter
-    uint32_t j; // loop counter
-    uint32_t k; // loop counter
+#define BASIC_VERSION // if used don't forget to also use the undefine at end of file
+#ifdef BASIC_VERSION
 
-    for (i = 0; i < M; i++) {
-        for (k = 0; k < O; k++) {
+    uint32_t m; // loop counter
+    uint32_t n; // loop counter
+    uint32_t o; // loop counter
+
+    for (m = 0; m < M; m++) {
+        for (o = 0; o < O; o++) {
             int32_t sum = 0;
-            for (j = 0; j < N; j++) {
-                sum = sum + pSrcA[i * N + j] * pSrcB[k * N + j];
+            for (n = 0; n < N; n++) {
+                sum = sum + pSrcA[m * N + n] * pSrcB[o * N + n];
             }
-            pDstC[i * O + k] = sum;
+            pDstC[m * O + o] = sum;
         }
     }
-}
 
 #else
 
-void plp_mat_mult_trans_i8v_xpulpv2(const int8_t *__restrict__ pSrcA,
-                                    const int8_t *__restrict__ pSrcB,
-                                    uint32_t M,
-                                    uint32_t N,
-                                    uint32_t O,
-                                    int32_t *__restrict__ pDstC) {
-
-    uint32_t i; // loop counter
-    uint32_t j; // loop counter
-    uint32_t k; // loop counter
-
-    uint32_t mod = N & 0x7;
-
-    if (mod == 7) {
-        for (i = 0; i < M; i++) {
-            for (k = 0; k < O; k++) {
-                int32_t sum1 = 0;
-                int32_t sum2 = 0;
-                for (j = 0; j < N / 8; j++) {
-                    v4s aVec1 = *((v4s *)&(pSrcA[i * N + j * 8]));
-                    v4s bVec1 = *((v4s *)&(pSrcB[k * N + j * 8]));
-                    v4s aVec2 = *((v4s *)&(pSrcA[i * N + j * 8 + 4]));
-                    v4s bVec2 = *((v4s *)&(pSrcB[k * N + j * 8 + 4]));
-
-                    sum1 = __SUMDOTP4(aVec1, bVec1, sum1);
-                    sum2 = __SUMDOTP4(aVec2, bVec2, sum2);
-                }
-                v4s aRem = *((v4s *)&(pSrcA[i * N + j * 8]));
-                v4s bRem = *((v4s *)&(pSrcB[k * N + j * 8]));
-                sum1 = __SUMDOTP4(aRem, bRem, sum1);
-                sum1 += pSrcA[i * N + j * 8 + 4] * pSrcB[k * N + j * 8 + 4];
-                sum1 += pSrcA[i * N + j * 8 + 5] * pSrcB[k * N + j * 8 + 5];
-                sum1 += pSrcA[i * N + j * 8 + 6] * pSrcB[k * N + j * 8 + 6];
-                pDstC[i * O + k] = sum1 + sum2;
-            }
-        }
-    } else if (mod == 6) {
-        for (i = 0; i < M; i++) {
-            for (k = 0; k < O; k++) {
-                int32_t sum1 = 0;
-                int32_t sum2 = 0;
-                for (j = 0; j < N / 8; j++) {
-                    v4s aVec1 = *((v4s *)&(pSrcA[i * N + j * 8]));
-                    v4s bVec1 = *((v4s *)&(pSrcB[k * N + j * 8]));
-                    v4s aVec2 = *((v4s *)&(pSrcA[i * N + j * 8 + 4]));
-                    v4s bVec2 = *((v4s *)&(pSrcB[k * N + j * 8 + 4]));
-
-                    sum1 = __SUMDOTP4(aVec1, bVec1, sum1);
-                    sum2 = __SUMDOTP4(aVec2, bVec2, sum2);
-                }
-                v4s aRem = *((v4s *)&(pSrcA[i * N + j * 8]));
-                v4s bRem = *((v4s *)&(pSrcB[k * N + j * 8]));
-                sum1 = __SUMDOTP4(aRem, bRem, sum1);
-                sum1 += pSrcA[i * N + j * 8 + 4] * pSrcB[k * N + j * 8 + 4];
-                sum1 += pSrcA[i * N + j * 8 + 5] * pSrcB[k * N + j * 8 + 5];
-                pDstC[i * O + k] = sum1 + sum2;
-            }
-        }
-    } else if (mod == 5) {
-        for (i = 0; i < M; i++) {
-            for (k = 0; k < O; k++) {
-                int32_t sum1 = 0;
-                int32_t sum2 = 0;
-                for (j = 0; j < N / 8; j++) {
-                    v4s aVec1 = *((v4s *)&(pSrcA[i * N + j * 8]));
-                    v4s bVec1 = *((v4s *)&(pSrcB[k * N + j * 8]));
-                    v4s aVec2 = *((v4s *)&(pSrcA[i * N + j * 8 + 4]));
-                    v4s bVec2 = *((v4s *)&(pSrcB[k * N + j * 8 + 4]));
-
-                    sum1 = __SUMDOTP4(aVec1, bVec1, sum1);
-                    sum2 = __SUMDOTP4(aVec2, bVec2, sum2);
-                }
-                v4s aRem = *((v4s *)&(pSrcA[i * N + j * 8]));
-                v4s bRem = *((v4s *)&(pSrcB[k * N + j * 8]));
-                sum1 = __SUMDOTP4(aRem, bRem, sum1);
-                sum1 += pSrcA[i * N + j * 8 + 4] * pSrcB[k * N + j * 8 + 4];
-                pDstC[i * O + k] = sum1 + sum2;
-            }
-        }
-    } else if (mod == 4) {
-        for (i = 0; i < M; i++) {
-            for (k = 0; k < O; k++) {
-                int32_t sum1 = 0;
-                int32_t sum2 = 0;
-                for (j = 0; j < N / 8; j++) {
-                    v4s aVec1 = *((v4s *)&(pSrcA[i * N + j * 8]));
-                    v4s bVec1 = *((v4s *)&(pSrcB[k * N + j * 8]));
-                    v4s aVec2 = *((v4s *)&(pSrcA[i * N + j * 8 + 4]));
-                    v4s bVec2 = *((v4s *)&(pSrcB[k * N + j * 8 + 4]));
-
-                    sum1 = __SUMDOTP4(aVec1, bVec1, sum1);
-                    sum2 = __SUMDOTP4(aVec2, bVec2, sum2);
-                }
-                v4s aRem = *((v4s *)&(pSrcA[i * N + j * 8]));
-                v4s bRem = *((v4s *)&(pSrcB[k * N + j * 8]));
-                sum1 = __SUMDOTP4(aRem, bRem, sum1);
-                pDstC[i * O + k] = sum1 + sum2;
-            }
-        }
-    } else if (mod == 3) {
-        for (i = 0; i < M; i++) {
-            for (k = 0; k < O; k++) {
-                int32_t sum1 = 0;
-                int32_t sum2 = 0;
-                for (j = 0; j < N / 8; j++) {
-                    v4s aVec1 = *((v4s *)&(pSrcA[i * N + j * 8]));
-                    v4s bVec1 = *((v4s *)&(pSrcB[k * N + j * 8]));
-                    v4s aVec2 = *((v4s *)&(pSrcA[i * N + j * 8 + 4]));
-                    v4s bVec2 = *((v4s *)&(pSrcB[k * N + j * 8 + 4]));
-
-                    sum1 = __SUMDOTP4(aVec1, bVec1, sum1);
-                    sum2 = __SUMDOTP4(aVec2, bVec2, sum2);
-                }
-                sum1 += pSrcA[i * N + j * 8] * pSrcB[k * N + j * 8];
-                sum1 += pSrcA[i * N + j * 8 + 1] * pSrcB[k * N + j * 8 + 1];
-                sum1 += pSrcA[i * N + j * 8 + 2] * pSrcB[k * N + j * 8 + 2];
-                pDstC[i * O + k] = sum1 + sum2;
-            }
-        }
-    } else if (mod == 2) {
-        for (i = 0; i < M; i++) {
-            for (k = 0; k < O; k++) {
-                int32_t sum1 = 0;
-                int32_t sum2 = 0;
-                for (j = 0; j < N / 8; j++) {
-                    v4s aVec1 = *((v4s *)&(pSrcA[i * N + j * 8]));
-                    v4s bVec1 = *((v4s *)&(pSrcB[k * N + j * 8]));
-                    v4s aVec2 = *((v4s *)&(pSrcA[i * N + j * 8 + 4]));
-                    v4s bVec2 = *((v4s *)&(pSrcB[k * N + j * 8 + 4]));
-
-                    sum1 = __SUMDOTP4(aVec1, bVec1, sum1);
-                    sum2 = __SUMDOTP4(aVec2, bVec2, sum2);
-                }
-                sum1 += pSrcA[i * N + j * 8] * pSrcB[k * N + j * 8];
-                sum1 += pSrcA[i * N + j * 8 + 1] * pSrcB[k * N + j * 8 + 1];
-                pDstC[i * O + k] = sum1 + sum2;
-            }
-        }
-    } else if (mod == 1) {
-        for (i = 0; i < M; i++) {
-            for (k = 0; k < O; k++) {
-                int32_t sum1 = 0;
-                int32_t sum2 = 0;
-                for (j = 0; j < N / 8; j++) {
-                    v4s aVec1 = *((v4s *)&(pSrcA[i * N + j * 8]));
-                    v4s bVec1 = *((v4s *)&(pSrcB[k * N + j * 8]));
-                    v4s aVec2 = *((v4s *)&(pSrcA[i * N + j * 8 + 4]));
-                    v4s bVec2 = *((v4s *)&(pSrcB[k * N + j * 8 + 4]));
-
-                    sum1 = __SUMDOTP4(aVec1, bVec1, sum1);
-                    sum2 = __SUMDOTP4(aVec2, bVec2, sum2);
-                }
-                sum1 += pSrcA[i * N + j * 8] * pSrcB[k * N + j * 8];
-                pDstC[i * O + k] = sum1 + sum2;
-            }
-        }
-    } else {
-        for (i = 0; i < M; i++) {
-            for (k = 0; k < O; k++) {
-                int32_t sum1 = 0;
-                int32_t sum2 = 0;
-                for (j = 0; j < N / 8; j++) {
-                    v4s aVec1 = *((v4s *)&(pSrcA[i * N + j * 8]));
-                    v4s bVec1 = *((v4s *)&(pSrcB[k * N + j * 8]));
-                    v4s aVec2 = *((v4s *)&(pSrcA[i * N + j * 8 + 4]));
-                    v4s bVec2 = *((v4s *)&(pSrcB[k * N + j * 8 + 4]));
-
-                    sum1 = __SUMDOTP4(aVec1, bVec1, sum1);
-                    sum2 = __SUMDOTP4(aVec2, bVec2, sum2);
-                }
-                pDstC[i * O + k] = sum1 + sum2;
-            }
-        }
-    }
-}
+    // TODO Hackathon
 
 #endif
+#undef BASIC_VERSION
+}
 
-// undefine BASIC_VERSION
 /**
    @} end of MatMultTransKernels group
 */
