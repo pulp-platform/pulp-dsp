b469c63e8f7a08392b869dd3a9008ece327e4802
Hanna Mueller <hanmuell@federispitz.ee.ethz.ch>
2020-08-03 16:23:28 +0200

    Optimizes matrix add/sub
    - does loop unrolling if defined so
    (surprisingly not faster in paralell implementation in testcases)
    - sees it as 1d problem (uses only one loop with +1 increasement for
    making HW-loop usage poosible)

--- src/MatrixFunctions/mat_sub/kernels/plp_mat_sub_f32p_xpulpv2.c
+++ src/MatrixFunctions/mat_sub/kernels/plp_mat_sub_f32p_xpulpv2.c
@@ -60,23 +60,38 @@ void plp_mat_sub_f32p_xpulpv2(void *args) {
     uint32_t nPE = a->nPE;
     float *__restrict__ pDst = a->pDst;
 
-#define BASIC_VERSION // if used don't forget to also use the undefine at end of file
-#ifdef BASIC_VERSION
-
-    uint32_t m, n; // loop counters
-
-    for (m = core_id; m < M; m += nPE) {
-        for (n = 0; n < N; n++) {
-            pDst[m * N + n] = pSrcA[m * N + n] - pSrcB[m * N + n];
-        }
+    uint32_t i; // loop counters
+    uint32_t total = M*N; // we can see it as a 1D operation
+#if defined(PLP_MATH_LOOPUNROLL)
+    // amount of elements per core, rounded up to next even number
+    uint32_t per_core = ((total+nPE-1)/nPE + 1) & 0xFFFFFFFE;
+    // compute the last element of the area each core has to process
+    uint32_t upper_bound = (core_id+1)*per_core;
+    // as we always rounded up before (to distribute the load as equal as possible) we need to check if the upper bound is still in our matrix
+    if(upper_bound > total ) upper_bound = total; 
+    // loop over the area assigned to the core - the shift by one is for the loop unrolling
+    for (i = core_id*(per_core>>1); i < (upper_bound>>1); i++) {
+            pDst[2*i] = pSrcA[2*i] - pSrcB[2*i];
+            pDst[2*i+1] = pSrcA[2*i+1] - pSrcB[2*i+1];
     }
-
-#else
-
-    // TODO: Hackathon
-
-#endif
-#undef BASIC_VERSION
+    
+    // to save the branch we just compute the possibly remaining element always and with all cores
+    // might lead to wait cycles due to contention while writing the same element
+    // possible improvement 1: last core has least work to do if there is a remaining element, make use of this
+    // possible improvement 2: if the cores that don't compute flush the pipeline, it should not be a waste of time, make use of this
+    pDst[total - 1] = pSrcA[total - 1] - pSrcB[total - 1];
+#else // No PLP_MATH_LOOPUNROLL
+    // amount of elements per core, rounded up
+    uint32_t per_core = (total+nPE-1)/nPE;
+    // compute the last element of the area each core has to process
+    uint32_t upper_bound = (core_id+1)*per_core;
+    // as we always rounded up before (to distribute the load as equal as possible) we need to check if the upper bound is still in our matrix
+    if(upper_bound > total ) upper_bound = total; 
+    // loop over the area assigned to the core
+    for (i = core_id*per_core; i < upper_bound; i++) {
+            pDst[i] = pSrcA[i] - pSrcB[i];
+    }
+#endif // PLP_MATH_LOOPUNROLL
 }
 
 /**
