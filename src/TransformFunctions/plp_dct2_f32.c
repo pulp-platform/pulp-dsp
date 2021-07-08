c748417dc407f4e8a87774de8b2824ce99a5827f
Aron Vince Szakacs <aszakacs@ethz.ch>
2021-07-08 18:06:46 +0200

    added mfcc, dct2

---
+++ src/TransformFunctions/plp_dct2_f32.c
@@ -0,0 +1,105 @@
+/* ----------------------------------------------------------------------
+ * Project:      PULP DSP Library
+ * Title:        plp_dct2_f32.c
+ * Description:  Floating-point DCT of type 2 on real input data
+ *
+ * $Date:        04. July 2021
+ * $Revision:    V0
+ *
+ * Target Processor: PULP cores with "F" support (wolfe, vega)
+ * -------------------------------------------------------------------- */
+/*
+ * Copyright (C) 2019 ETH Zurich and University of Bologna. All rights reserved.
+ *
+ * Author: Aron Szakacs, ETH Zürich
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "plp_math.h"
+
+/**
+  @ingroup groupTransforms
+ */
+
+/**
+  @defgroup fft  FFT transforms
+  This module contains the code to perform FFT transforms.
+
+
+ */
+
+/**
+   @addtogroup fft
+   @{
+*/
+
+/**
+   @brief Floating-point DCT on real input data. Implementation of
+   			John Makhoul's "A Fast Cosine Transform in One
+			and Two Dimensions" 1980 IEEE paper.
+   @param[in]   S       points to an instance of the floating-point FFT
+   			structure with FFTLength = DCTLength
+   @param[in]   pShift  points to twiddle coefficient table of 4*FFTLength,
+   			of which only the first quarter is necessary.
+   @param[in]   pSrc    points to the input buffer (real data) of size
+			FFTLength
+   @param[out]  pBuf    points to buffer of size 2*FFTLength, used for 
+   			computation.
+   @param[out]  pDst    points to output buffer (real data) of size FFTLength,
+   			may be the same as pSrc.
+   @return      none
+*/
+void plp_dct2_f32(const plp_fft_instance_f32 *S,
+                  const Complex_type_f32 *__restrict__ pShift,
+                  const float32_t *__restrict__ pSrc,
+                  float32_t *__restrict__ pBuf,
+                  float32_t *__restrict__ pDst) {
+
+    // 1: reordering (moves input from pSrc to pDst)
+    uint32_t N = S->FFTLength;
+    // write odd indices from Src into buffer
+    for (int i=0;i<N/2;i++){
+	pBuf[i] = pSrc[2*i + 1];
+    }
+    // squeeze remaining even indices closer together (decimate signal to odd indices) and write into Dst
+    for (int i=0;i<N/2;i++){
+	pDst[i] = pSrc[2*i];
+    }
+    // reverse and write saved odd indices into second half of Dst
+    for (int i=0;i<N/2;i++){
+	pDst[N/2+i] = pBuf[N/2-1-i];
+    }
+    // 2: RFFT of reordered sequence, result written into buffer
+    plp_rfft_f32(S, pDst, pBuf);
+    // RFFT must be extended to all FFTLength complex coefficients,
+    // using X[k] = X*[-k] for real x[t]
+    for (int i=0;i<N/2-1;i++) {
+	pBuf[2*(N/2+1+i)] = pBuf[2*(N/2-1-i)]; 
+	pBuf[2*(N/2+1+i)+1] = (-1)*pBuf[2*(N/2-1-i)+1];
+    }
+    // 3: shift FFT in place in buffer
+    plp_cmplx_mult_cmplx_f32(pBuf, (float32_t*)pShift, pBuf, N);
+    // 4: take real part, moving from buffer to output
+    for (int i=0;i<N;i++){
+	pDst[i] = pBuf[2*i];
+    }
+    // 5: multiply by 2 in place in output
+    plp_scale_f32(pDst, 2, pDst, N);
+}
+
+/**
+   @} end of FFT group
+*/
---
+++ src/TransformFunctions/plp_mfcc_f32.c
@@ -0,0 +1,162 @@
+/* ----------------------------------------------------------------------
+ * Project:      PULP DSP Library
+ * Title:        plp_mfcc_f32.c
+ * Description:  Floating-point MFCC on real input data
+ *
+ * $Date:        05. July 2021
+ * $Revision:    V0
+ *
+ * Target Processor: PULP cores with "F" support (wolfe, vega)
+ * -------------------------------------------------------------------- */
+/*
+ * Copyright (C) 2021 ETH Zurich and University of Bologna. All rights reserved.
+ *
+ * Author: Aron Szakacs, ETH Zurich
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "plp_math.h"
+#define PI 3.14159265358979323846
+void print_vec_t(float32_t *V, int n)
+{
+  printf("[");
+  for(int i=0;i<7;i++)
+  {
+    printf("%d: ", i);
+    printf("%e, ", V[i]);
+  }
+  printf("... ");
+  for(int i=7;i>0;i--)
+  {
+    printf("%d: ", n-i);
+    printf("%e, ", V[n-i]);
+  }
+  printf("]\n");
+}
+
+
+/**
+  @ingroup groupTransforms
+ */
+
+/**
+  @defgroup fft  FFT transforms
+  This module contains the code to perform FFT transforms.
+
+
+ */
+
+/**
+   @addtogroup fft
+   @{
+*/
+
+/**
+   @brief MFCC on real input data.
+   @param[in]   SFFT        points to an instance of the floating-point FFT
+   			    structure for the initial FFT (with FFTLength = n_fft).
+			    bitReverseFlag should be on.
+   @param[in]   SDCT        points to an instance of the floating-point FFT
+   			    structure for the DCT (with FFTLength = n_mels).
+			    bitReverseFlag should be on.
+   @param[in]   pShift      points to twiddle coefficient table with
+   			    FFTLength = 4*n_mels. Only first quarter necessary.
+   @param[in]   filterBank  points to plp_triangular_filter_f32 instance with 
+   			    nFilters = n_mels.
+   @param[in]   pSrc        points to the input buffer (real data, size n_fft)
+   @param[out]  pDst        points to the output buffer (complex data).
+   			    Must be of length at least 3*n_fft.
+			    pSrc and pDst must not overlap, the calculation can
+			    not be done in place. 
+			    MFCCs are returned in the first n_mels spots.
+   @return      none
+*/
+void plp_mfcc_f32(const plp_fft_instance_f32 *SFFT,
+                  const plp_fft_instance_f32 *SDCT,
+		  const Complex_type_f32 *__restrict__ pShift,
+		  const plp_triangular_filter_f32 *__restrict__ filterBank,
+                  const float32_t *__restrict__ pSrc,
+                  float32_t *__restrict__ pDst) {
+
+
+	// Step 0: Periodic Hann windowing (could be left to user, 
+	// also should be precomputed).
+	// Stored in buffer space of pDst.
+	uint32_t n_fft = SFFT->FFTLength;
+	float32_t *fft_in = pDst + 2*n_fft;
+	for (int i=0;i<n_fft;i++){
+		fft_in[i] = pSrc[i]*(0.5f*(1-plp_cos_f32(2*PI*i/(n_fft))));
+	}
+	printf("AFTER HANN, BEFORE FFT\n"); //remove!
+	print_vec_t(fft_in, n_fft); //remove!
+	
+
+	// Step 1: FFT
+	plp_rfft_f32(SFFT, fft_in, pDst);
+	printf("AFTER FFT, BEFORE MAG\n"); //remove!
+	print_vec_t(pDst, n_fft+2); //remove!
+	
+
+	// Step 2: ||.||^2 of each RFFT point / Take squared magnitude.
+	// Stores result in free buffer space of pDst, right behind
+	// the RFFT's (n_fft+2)-long output. 
+	float32_t *fft_mag = pDst + n_fft+2;
+	plp_cmplx_mag_squared_f32(pDst, fft_mag, n_fft/2 + 1);
+	printf("AFTER MAG, BEFORE FB\n"); //remove!
+	print_vec_t(fft_mag, n_fft/2 + 1); //remove!
+
+
+	// Step 3: Apply triangular filter bank.
+	// results are stored in the beginning of pDst.
+	float32_t *fb_out = pDst;
+	uint16_t n_mels = filterBank->nFilters;
+	float32_t *filter_start = (float32_t*)filterBank->V;
+	for (int i=0;i<n_mels;i++){
+		uint16_t current_length = filterBank->filterLength[i];
+		plp_dot_prod_f32(
+                          fft_mag+filterBank->firstValue[i], 
+                          filter_start,
+                          current_length, 
+                          fb_out+i);
+		filter_start += current_length;
+  	}
+	printf("AFTER FB, BEFORE LOG\n"); //remove!
+	print_vec_t(fb_out, n_mels); //remove!
+
+
+	// Step 4: Take the log of the computed mel scale. (what log..?)
+	plp_offset_f32(fb_out, 1e-6f, fb_out, 32);
+	float32_t *mel_logs = fb_out;
+	for (int i=0;i<n_mels;i++){
+        	float32_t log_i = log(fb_out[i]);
+        	mel_logs[i] = log_i;
+        	//fft_h_out[n_dct-1-i] = log_i;	// why tf did I do this?
+	}
+	printf("AFTER LOG, BEFORE DCT\n"); //remove!
+	print_vec_t(fb_out, n_mels); //remove!
+
+
+	// Step 5: DCT of log mels
+	float32_t *dct_inout = mel_logs;
+	float32_t *dct_buffer = pDst + n_mels;
+	plp_dct2_f32(SDCT, pShift, dct_inout, dct_buffer, dct_inout);
+	printf("AFTER DCT\n"); //remove!
+	print_vec_t(dct_inout, n_mels); //remove!
+}
+
+/**
+   @} end of FFT group
+*/
