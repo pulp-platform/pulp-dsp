4d46736b594dfaffaedef60e5ea2790be0849b1c
Jakub Mandula <jakub@mandula.cz>
2021-07-16 20:55:34 +0200

    Generate more complete Wavelet Tables

--- include/plp_const_structs.h
+++ include/plp_const_structs.h
@@ -56,11 +56,64 @@ extern const plp_fft_instance_f32 plp_rfft_sR_f32_len2048;
 extern const plp_triangular_filter_f32 plp_triangular_filter_f32_32;
 
 
-extern const plp_dwt_wavelet_f32 PLP_DWT_HAAR_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF1_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF2_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF3_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF4_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF5_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF6_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF7_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF8_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF9_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF10_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF11_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF12_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF13_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF14_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF15_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF16_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_COIF17_f32;
 extern const plp_dwt_wavelet_f32 PLP_DWT_DB1_f32;
 extern const plp_dwt_wavelet_f32 PLP_DWT_DB2_f32;
 extern const plp_dwt_wavelet_f32 PLP_DWT_DB3_f32;
 extern const plp_dwt_wavelet_f32 PLP_DWT_DB4_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB5_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB6_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB7_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB8_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB9_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB10_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB11_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB12_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB13_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB14_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB15_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB16_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB17_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB18_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB19_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_DB20_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_HAAR_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM2_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM3_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM4_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM5_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM6_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM7_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM8_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM9_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM10_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM11_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM12_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM13_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM14_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM15_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM16_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM17_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM18_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM19_f32;
+extern const plp_dwt_wavelet_f32 PLP_DWT_SYM20_f32;
+
 
 
 
