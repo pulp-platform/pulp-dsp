86083b925c1f4bd5059d7bc511a8e2426bc8cbdf
Michael Rogenmoser <michael@rogenmoser.us>
2021-08-03 14:14:00 +0200

    Update test for cmplx_mag to ensure proper scaling, edit doc

--- src/ComplexMathFunctions/plp_cmplx_mag_q16.c
+++ src/ComplexMathFunctions/plp_cmplx_mag_q16.c
@@ -30,6 +30,37 @@
 #include "plp_math.h"
 
 /**
+  @ingroup groupCmplxMath
+ */
+
+/**
+  @defgroup cmplx_mag Complex Magnitude
+  Computes the magnitude of the elements of a complex data vector.
+  The <code>pSrc</code> points to the source data and
+  <code>pDst</code> points to the where the result should be written.
+  <code>numSamples</code> specifies the number of complex samples
+  in the input array and the data is stored in an interleaved fashion
+  (real, imag, real, imag, ...).
+  The input array has a total of <code>2*numSamples</code> values;
+  the output array has a total of <code>numSamples</code> values.
+  The underlying algorithm is used:
+  <pre>
+  for (n = 0; n < numSamples; n++) {
+      pDst[n] = sqrt(pSrc[(2*n)+0]^2 + pSrc[(2*n)+1]^2);
+  }
+  </pre>
+  There are separate functions for floating point, integer, and fixed point 32- 16- 8-bit data
+  types. Please ensure the appropriate range is used for integers and fixed point data types, as
+  these are at risk for overflow if using too large numbers. Larger than normal errors are possible
+  when using fixed point numbers and very small values.
+ */
+
+/**
+  @addtogroup cmplx_mag
+  @{
+ */
+
+/**
  * @brief      Glue code for complex magnitude calculation in 16-bit quantized integer
  *
  * @param[in]  pSrc        pointer to source
@@ -50,3 +81,7 @@ void plp_cmplx_mag_q16(const int16_t *pSrc,
         plp_cmplx_mag_q16s_xpulpv2(pSrc, fracBits, pRes, numSamples);
     }
 }
+
+/**
+  @} end of cmplx_mag
+ */
