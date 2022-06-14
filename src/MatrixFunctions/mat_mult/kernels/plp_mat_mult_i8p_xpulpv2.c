3434f32a6a2698dc46873635229c05530d3f9495
Emmet <murphe67@tcd.ie>
2022-06-14 02:03:05 -0700

    Move matrix b data shuffle outside of computation loop

--- src/MatrixFunctions/mat_mult/kernels/plp_mat_mult_i8p_xpulpv2.c
+++ src/MatrixFunctions/mat_mult/kernels/plp_mat_mult_i8p_xpulpv2.c
@@ -107,6 +107,26 @@ void plp_mat_mult_i8p_xpulpv2(void *args) {
     uint32_t core_id = hal_core_id();
 
     for (k = core_id; k < O / 4; k += nPE) {
+
+        v4s bVecs[(N/4)*4];
+
+        for(j = 0; j < N / 4; j++){
+            v4s temp0 = *((v4s *)&(pSrcB[(j * 4) * O + (k * 4)]));
+            v4s temp1 = *((v4s *)&(pSrcB[(j * 4 + 1) * O + (k * 4)]));
+            v4s temp2 = *((v4s *)&(pSrcB[(j * 4 + 2) * O + (k * 4)]));
+            v4s temp3 = *((v4s *)&(pSrcB[(j * 4 + 3) * O + (k * 4)]));
+
+            v4s temp4 = __builtin_shuffle(temp0, temp1, mask0); // 0,1,4,5
+            v4s temp5 = __builtin_shuffle(temp2, temp3, mask0); // 8,9,12,13
+            v4s temp6 = __builtin_shuffle(temp0, temp1, mask1); // 2,3,6,7
+            v4s temp7 = __builtin_shuffle(temp2, temp3, mask1); // 3,7,11,15
+
+            bVecs[(j * 4)] = __builtin_shuffle(temp4, temp5, mask2);
+            bVecs[(j * 4) + 1] = __builtin_shuffle(temp4, temp5, mask3);
+            bVecs[(j * 4) + 2] = __builtin_shuffle(temp6, temp7, mask2);
+            bVecs[(j * 4) + 3] = __builtin_shuffle(temp6, temp7, mask3);
+        }
+
         for (i = 0; i < M / 2; i++) {
 
             int32_t sum00 = 0;
@@ -125,20 +145,10 @@ void plp_mat_mult_i8p_xpulpv2(void *args) {
                 v4s aVec0 = *((v4s *)&(pSrcA[(i * 2) * N + (j * 4)]));
                 v4s aVec1 = *((v4s *)&(pSrcA[(i * 2 + 1) * N + (j * 4)]));
 
-                v4s temp0 = *((v4s *)&(pSrcB[(j * 4) * O + (k * 4)]));
-                v4s temp1 = *((v4s *)&(pSrcB[(j * 4 + 1) * O + (k * 4)]));
-                v4s temp2 = *((v4s *)&(pSrcB[(j * 4 + 2) * O + (k * 4)]));
-                v4s temp3 = *((v4s *)&(pSrcB[(j * 4 + 3) * O + (k * 4)]));
-
-                v4s temp4 = __builtin_shuffle(temp0, temp1, mask0); // 0,1,4,5
-                v4s temp5 = __builtin_shuffle(temp2, temp3, mask0); // 8,9,12,13
-                v4s temp6 = __builtin_shuffle(temp0, temp1, mask1); // 2,3,6,7
-                v4s temp7 = __builtin_shuffle(temp2, temp3, mask1); // 3,7,11,15
-
-                v4s bVec0 = __builtin_shuffle(temp4, temp5, mask2);
-                v4s bVec1 = __builtin_shuffle(temp4, temp5, mask3);
-                v4s bVec2 = __builtin_shuffle(temp6, temp7, mask2);
-                v4s bVec3 = __builtin_shuffle(temp6, temp7, mask3);
+                v4s bVec0 = bVecs[(j * 4)];
+                v4s bVec1 = bVecs[(j * 4) + 1];
+                v4s bVec2 = bVecs[(j * 4) + 2];
+                v4s bVec3 = bVecs[(j * 4) + 3];
 
                 sum00 = __SUMDOTP4(aVec0, bVec0, sum00);
                 sum01 = __SUMDOTP4(aVec0, bVec1, sum01);
@@ -173,7 +183,7 @@ void plp_mat_mult_i8p_xpulpv2(void *args) {
         uint32_t jEnd = j;
         uint32_t kEnd = k >= O ? O : k;
 
-        // clean up for j
+        //clean up for j
         if (jEnd != N) {
             for (k = core_id * 4; k < kEnd; k += nPE * 4) {
                 for (int step = 0; step < 4; step++) {
