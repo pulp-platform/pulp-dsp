240940b040a4454887eece075ddf8120d9744a7f
Marco Bertuletti <mbertuletti@iis.ee.ethz.ch>
2022-07-18 15:22:04 +0200

    [src] Parallelize matrix inversion

--- src/MatrixFunctions/mat_inv/kernels/plp_mat_inv_f32p_xpulpv2.c
+++ src/MatrixFunctions/mat_inv/kernels/plp_mat_inv_f32p_xpulpv2.c
@@ -11,7 +11,7 @@
 /*
  * Copyright (C) 2020 ETH Zurich and University of Bologna.
  *
- * Author: Tibor Schneider, ETH Zurich
+ * Author: Marco Bertuletti, ETH Zurich
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -57,18 +57,17 @@ int plp_mat_inv_f32p_xpulpv2(void *args) {
     uint32_t *flag = a -> flag;
     uint32_t N = a -> N;
     uint32_t nPE = a -> nPE;
+    float *pSrcT1, *pSrcT2;                         /* Temporary input data matrix pointer */
+    float *pDstT1, *pDstT2;                         /* Temporary output data matrix pointer */
+    float *pPivotRowIn;                             /* Temporary input and output data matrix pointer */
+    float *pPRT_in, *pPivotRowDst, *pPRT_pDst;      /* Temporary input and output data matrix pointer */
+    float Xchg, in = 0.0f, in1;                     /* Temporary input values  */
+    uint32_t i, rowCnt, j, loopCnt, k, l;   /* loop counters */
+    uint32_t M = N;                         /* M is the number of rows. However, the matirces must be square. */
+    pDstT1 = pDst;                          /* Working pointer for destination matrix */
 
-    float *pSrcT1, *pSrcT2;                    /* Temporary input data matrix pointer */
-    float *pDstT1, *pDstT2;                    /* Temporary output data matrix pointer */
-    float *pPivotRowIn;                        /* Temporary input and output data matrix pointer */
-    float *pPRT_in, *pPivotRowDst, *pPRT_pDst; /* Temporary input and output data matrix pointer */
-    float Xchg, in = 0.0f, in1;                      /* Temporary input values  */
-    uint32_t i, rowCnt, j, loopCnt, k, l; /* loop counters */
-    uint32_t M = N; /* M is the number of rows. However, the matirces must be square. */
+    /* CREATE IDENTITY MATRIX */
 
-    
-    pDstT1 = pDst; /* Working pointer for destination matrix */
-    /* Making the destination matrix as identity matrix */
     for (int i = core_id; i < M; i += nPE) {
         for (int j = 0; j < M; j++) {
             pDstT1[i * M + j] = (float)(i == j);
@@ -79,83 +78,59 @@ int plp_mat_inv_f32p_xpulpv2(void *args) {
     /* Loop over the number of columns of the input matrix.
        All the elements in each column are processed by the row operations */
     loopCnt = N;
-
-    /* Index modifier to navigate through the columns */
-    l = 0U;
-
+    l = 0U; // Column index
     while (loopCnt > 0U) {
 
-        /* Check if the pivot element is zero..
-         * If it is zero then interchange the row with non zero row below.
-         * If there is no non zero element to replace in the rows below,
-         * then the matrix is Singular. */
+        /* SEARCH FOR 0 AND SWITCH ROWS */
 
-        /* Working pointer for the input matrix that points
-         * to the pivot element of the particular row  */
-        pSrcT1 = pSrc + (l * N);
-        /* Working pointer for the destination matrix that points
-         * to the pivot element of the particular row  */
-        pDstT1 = pDst + (l * N);
-        /* Temporary variable to hold the pivot value */
+        pSrcT1 = pSrc + (l * N); // Input pivot
+        pDstT1 = pDst + (l * N); // Destination pivot
+
         in = *pSrcT1;
-
-
-        if (core_id == 0) {
-            // SEARCH FOR 0 AND SWITCH ROWS //  
-
-            /* Destination pointer modifier */
-            k = 1U;
-            /* Check if the pivot element is zero */
-            if (*pSrcT1 == 0.0f) {
-                /* Loop over the number rows present below */
-                for (i = (l + 1U); i < M; i++) {
-                    /* Update the input and destination pointers */
-                    pSrcT2 = pSrcT1 + (N * i);
-                    pDstT2 = pDstT1 + (N * k);
-                    /* Check if there is a non zero pivot element to
-                     * replace in the rows below */
-                    if (*pSrcT2 != 0.0f) {
-                        /* Loop over number of columns
-                         * to the right of the pilot element */
-                        j = N - l;
-                        while (j > 0U) {
-                            /* Exchange the row elements of the input matrix */
-                            Xchg = *pSrcT2;
-                            *pSrcT2++ = *pSrcT1;
-                            *pSrcT1++ = Xchg;
-                            /* Decrement the loop counter */
-                            j--;
-                        }
-                        /* Loop over number of columns of the destination matrix */
-                        j = N;
-                        while (j > 0U) {
-                            /* Exchange the row elements of the destination matrix */
-                            Xchg = *pDstT2;
-                            *pDstT2++ = *pDstT1;
-                            *pDstT1++ = Xchg;
-                            /* Decrement loop counter */
-                            j--;
-                        }
-                        /* Flag to indicate whether exchange is done or not */
-                        *flag = 1U;
-                        /* Break after exchange is done */
-                        break;
+        k = 1U;
+        /* Check if the pivot element is zero */
+        if (*pSrcT1 == 0.0f) {
+            /* Loop over the number rows present below */
+            for (i = (l + 1U) + core_id; i < M; i += nPE) {
+                pSrcT2 = pSrcT1 + (N * i);
+                /* Check if there is element to exchange */
+                if (*pSrcT2 != 0.0f)
+                    *flag = k;
+                if (*flag != 0) {
+                    pSrcT2 = pSrcT1 + (N * *flag + l);
+                    pDstT2 = pDstT1 + (N * *flag);
+                    /* Loop over number of columns
+                     * to the right of the pilot element */
+                    for (j = core_id; j < N - l; j += nPE) {
+                        /* Exchange the row elements of the input matrix */
+                        Xchg = pSrcT2[j];
+                        pSrcT2[j] = pSrcT1[j];
+                        pSrcT1[j] = Xchg;
                     }
-                    /* Update the destination pointer modifier */
-                    k++;
+                    pSrcT1 += N - l;
+                    pSrcT2 += N - l;
+                    /* Loop over number of columns of the destination matrix */
+                    for(j = core_id; j < N; j += nPE) {
+                        /* Exchange the row elements of the destination matrix */
+                        Xchg = pDstT2[j];
+                        pDstT2[j] = pDstT1[j];
+                        pDstT1[j] = Xchg;
+                    }
+                    pDstT2 += N;
+                    pDstT1 += N;
+                    break;
                 }
             }
+            k++;
+            hal_team_barrier();
         }
-        hal_team_barrier();
 
         /* Update the status if the matrix is singular */
-        if ((*flag != 1U) && (in == 0.0f)) {
+        if ((*flag == 0U) && (in == 0.0f)) {
             return 1;
         }
 
-
-
-        // DIVIDE BY PIVOT
+        /* DIVIDE BY PIVOT */
 
         /* Points to the pivot row of input and destination matrices */
         pPivotRowIn = pSrc + (l * N);
@@ -165,45 +140,26 @@ int plp_mat_inv_f32p_xpulpv2(void *args) {
         pSrcT2 = pPivotRowDst;
         /* Pivot element of the row */
         in = *pPivotRowIn;
-        /* Loop over number of columns
-         * to the right of the pilot element */
-        for (j = core_id; j < (N-l); j += nPE) {
-            /* Divide each element of the row of the input matrix
-             * by the pivot element */
+        /* Loop over number of columns to the right of the pilot element */
+        for(j = core_id; j < N - l; j += nPE) {
             in1 = pSrcT1[j];
             pSrcT1[j] = in1 / in;
         }
         /* Loop over number of columns of the destination matrix */
-        for (j = core_id; j < N; j += nPE) {
-            /* Divide each element of the row of the destination matrix
-             * by the pivot element */
+        for(j = core_id; j < N; j += nPE) {
             in1 = pSrcT2[j];
             pSrcT2[j] = in1 / in;
         }
         hal_team_barrier();
 
+        /*REPLACE ROWS */
 
-
-        // SUM OF A MULTIPLE
-
-        /* Replace the rows with the sum of that row and a multiple of row i
-         * so that each new element in column i above row i is zero.*/
-        /* Temporary pointers for input and destination matrices */
-        pSrcT1 = pSrc;
-        pSrcT2 = pDst;
-
-        /* index used to check for pivot element */
-        i = 0U;
-        /* Loop over number of rows */
-        /* to be replaced by the sum of that row and a multiple of row i */
+        pSrcT1 = pSrc + core_id * N;
+        pSrcT2 = pDst + core_id * N;
+        i = core_id;
         k = M;
-        while (k > 0U) {
-
-            /* Only the columns to the right of the pivot are to be processed */
-            if (i == l) {
-                pSrcT1 += N - l;
-                pSrcT2 += N;
-            } else {
+        for(k = core_id; k < M; k += nPE) {
+            if (i != l) {
                 /* Element of the reference row */
                 in = *pSrcT1;
                 /* Working pointers for input and destination pivot rows */
@@ -211,50 +167,39 @@ int plp_mat_inv_f32p_xpulpv2(void *args) {
                 pPRT_pDst = pPivotRowDst;
                 /* Loop over the number of columns to the right of the pivot element,
                    to replace the elements in the input matrix */
-                j = (N - l);
-                for (j = core_id; j < (N-l); j += nPE) {
-                    /* Replace the element by the sum of that row
-                       and a multiple of the reference row  */
+                for (j = 0; j < N - l; j++) {
                     in1 = pSrcT1[j];
                     pSrcT1[j] = in1 - (in * pPRT_in[j]);
                 }
                 /* Loop over the number of columns to
                    replace the elements in the destination matrix */
-                for (j = core_id; j < N; j += nPE) {
-                    /* Replace the element by the sum of that row
-                       and a multiple of the reference row  */
+                for (j = 0; j < N; j++) {
                     in1 = pSrcT2[j];
                     pSrcT2[j] = in1 - (in * pPRT_pDst[j]);
                 }
             }
-            hal_team_barrier();
-
-            /* Increment temporary input pointer */
-            pSrcT1 = pSrcT1 + N;
-            /* Decrement loop counter */
-            k--;
-            /* Increment pivot index */
-            i++;
+            i += nPE;
+            pSrcT1 += nPE * N;
+            pSrcT2 += nPE * N;
         }
-
         /* Increment the input pointer */
         pSrc++;
         /* Decrement the loop counter */
         loopCnt--;
         /* Increment the index modifier */
         l++;
+        hal_team_barrier();
     }
-
-    if ((*flag != 1U) && (in == 0.0f)) {
+    
+    if ((*flag == 0) && (in == 0.0f)) {
         for (i = 0; i < M * N; i++) {
             if (pSrc[i] != 0.0f)
                 break;
         }
-
         if (i == M * N)
             return 1;
+        hal_team_barrier();
     }
-
     return 0;
 }
 
